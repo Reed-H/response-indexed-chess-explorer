e3636c63910230bc5d8b1508d6191b67055662a9
Reed Hoffmaster <36741999+Reed-H@users.noreply.github.com>
2026-03-01 18:13:09 -0600

    Improve clustered tree rendering, controls, and overlays

--- js/engine.js
+++ js/engine.js
@@ -28,4 +28,32 @@ export class Engine {
         };
     }
 
+    evaluateTopLines(fen, depth = 13, multiPv = 3, callback) {
+        const lines = new Map();
+        this.worker.postMessage('ucinewgame');
+        this.worker.postMessage('setoption name MultiPV value ' + multiPv);
+        this.worker.postMessage('position fen ' + fen);
+        this.worker.postMessage('go depth ' + depth);
+
+        this.worker.onmessage = (e) => {
+            const msg = e.data;
+            if (msg.startsWith('info') && msg.includes(' pv ') && msg.includes(' multipv ')) {
+                const mpv = msg.match(/multipv (\d+)/);
+                const cp = msg.match(/score cp (-?\d+)/);
+                const mate = msg.match(/score mate (-?\d+)/);
+                const pv = msg.match(/ pv (.+)$/);
+                if (mpv && pv) {
+                    const score = cp ? parseInt(cp[1], 10) / 100 : (mate ? `#${mate[1]}` : null);
+                    lines.set(Number(mpv[1]), { score, pv: pv[1] });
+                }
+            }
+
+            if (msg.startsWith('bestmove')) {
+                callback([...lines.entries()]
+                    .sort((a, b) => a[0] - b[0])
+                    .map(([, v]) => v)
+                    .slice(0, multiPv));
+            }
+        };
+    }
 }
